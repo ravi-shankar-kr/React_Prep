0dc40eedc49c3eaa50040a9a24effbe6f67d391c
Ravi_Shankar_kr <ravi896959@gmail.com>
2025-08-21 13:26:33 +0530

    Adding Tailwinnd CSS in Prepration project

--- prepration/src/App.jsx
+++ prepration/src/App.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
+import './app.css'
 
 const App = () => {
   const [a, setA] = useState(10)
   return (
     
-    <div>
-      <h1>number is {a}</h1>
-      <button onClick={() => setA(a + 1)}>Increment</button>
+    <div  >
+      <h1 className='text-5xl font-semibold' >number is: {a}</h1>
+      <button className='bg-blue-500 text-white py-2 px-4 rounded mx-2' onClick={() => setA(a + 1)}>Increment</button>
     </div>
   )
 }
